a4031a3939e32e131003fb7b6630a6e571403530
José Rodolpho <j.rodolpho29@hotmail.com>
2022-04-04 16:27:17 -0300

    History created

--- src/components/History/index.js
+++ src/components/History/index.js
@@ -1,13 +1,34 @@
 import Footer from "../Footer"
 import Header from "../Header"
-
+import styled from "styled-components"
 export default function History(){
+    return(
     <>
         <Header/>
-        <div>
+        <Text>
             <h2>Histórico</h2>
             <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>
-        </div>
+        </Text>
         <Footer/>
     </>
+    )
 }
+
+const Text = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 70px;
+    padding-top: 90px;
+    padding-left: 15px;
+    padding-right: 15px;
+
+    p{
+        color: #666666;
+    }
+
+    h2{
+        color: #126BA5;
+        font-size: 23px;
+    }
+`
